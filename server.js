400e96ac71015cd2d1903ddb89ac751147623e0a
Wolvan <wolvan1@gmail.com>
2018-01-07 16:56:44 +0100

    Allow remote calls
    As requested by @LiddleDev on gitter, this commit allows disabling the
    integrated security feature of only allowing localhost to connect to the
    ark-rpc server.
    
    This behavior has to explicitly be enabled by supplying `--allow-remote`
    as a commandline argument to the ark-rpc server.
    
    The README has been changed to reflect this change.

--- server.js
+++ server.js
@@ -9,12 +9,16 @@ var program = require('commander');
 var server = null;
 
 function restrictToLocalhost(req, res, next){
-  if(req.getRoute().path == '/:network/broadcast' || req.connection.remoteAddress == "::1" || req.connection.remoteAddress == "127.0.0.1" || req.connection.remoteAddress == "::ffff:127.0.0.1")
-    next();
-  else res.end();
+  if(program.allowRemote) next();
+  else
+    if(req.getRoute().path == '/:network/broadcast' || req.connection.remoteAddress == "::1" || req.connection.remoteAddress == "127.0.0.1" || req.connection.remoteAddress == "::ffff:127.0.0.1")
+      next();
+    else res.end();
 }
 
 function startServer(port){
+  if (program.allowRemote) console.log('Warning! ark-rpc allows remote connections, this is potentially insecure!');
+
   server = restify.createServer().
     use(restrictToLocalhost).
     use(restify.plugins.bodyParser({mapParams: true})).
@@ -37,6 +41,7 @@ function startServer(port){
 
 program.
   option('-p, --port <port>', 'The port to start server').
+  option('--allow-remote', 'Allow connections from sources other than localhost').
   parse(process.argv);
 
 if(program.port)
